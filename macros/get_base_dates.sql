f6f14de93c2bc74590fe5b45784f61fd9313becd
候高杰 <hgj02217875@alibaba-inc.com>
2025-04-08 17:27:55 +0800

    若用户使用宏时涉及输入字符串类型的日期，需自行进行显式转换，无需额外转换。删除多余的逗号。

--- macros/get_base_dates.sql
+++ macros/get_base_dates.sql
@@ -20,7 +20,7 @@ with date_spine as
     {{ dbt_date.date_spine(
         datepart=datepart,
         start_date=start_date,
-        end_date=end_date,
+        end_date=end_date
        )
     }}
 
@@ -49,7 +49,7 @@ with date_spine as
     {{ dbt_date.date_spine(
         datepart=datepart,
         start_date=start_date,
-        end_date=end_date,
+        end_date=end_date
        )
     }}
 
@@ -79,7 +79,7 @@ with date_spine as
     {{ dbt_date.date_spine(
         datepart=datepart,
         start_date=start_date,
-        end_date=end_date,
+        end_date=end_date
        )
     }}
 
@@ -93,17 +93,11 @@ from
 {% macro maxcompute__get_base_dates(start_date, end_date, n_dateparts, datepart) %}
 
 {%- if start_date and end_date -%}
-    {%- if start_date is string and end_date is string -%}
-    {%- set start_date=dbt.type_timestamp() ~ "'" ~ start_date ~ "'" -%}
-    {%- set end_date=dbt.type_timestamp() ~ "'" ~ end_date ~ "'" -%}
-    {%- else -%}
-    {%- set start_date=dbt.type_timestamp() ~ "'" ~ start_date.strftime('%Y-%m-%d %H:%M:%S') ~ "'" -%}
-    {%- set end_date=dbt.type_timestamp() ~ "'" ~ end_date.strftime('%Y-%m-%d %H:%M:%S') ~ "'" -%}
-    {%- endif -%}
+    {%- set start_date = start_date -%}
+    {%- set end_date = end_date -%}
 {%- elif n_dateparts and datepart -%}
-
-{%- set start_date = dbt.dateadd(datepart, -1 * n_dateparts, dbt_date.today()) -%}
-{%- set end_date = dbt_date.tomorrow() -%}
+    {%- set start_date = dbt.dateadd(datepart, -1 * n_dateparts, dbt_date.today()) -%}
+    {%- set end_date = dbt_date.tomorrow() -%}
 {%- endif -%}
 
 with date_spine as
@@ -112,7 +106,7 @@ with date_spine as
     {{ dbt_date.date_spine(
         datepart=datepart,
         start_date=start_date,
-        end_date=end_date,
+        end_date=end_date
        )
     }}
 
