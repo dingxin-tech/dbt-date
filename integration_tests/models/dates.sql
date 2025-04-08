85c5dc677c58a9bce50e1e26234006fae1103cf9
候高杰 <hgj02217875@alibaba-inc.com>
2025-04-08 17:27:42 +0800

    若用户使用宏时涉及输入字符串类型的日期，需自行进行显式转换。

--- integration_tests/models/dates.sql
+++ integration_tests/models/dates.sql
@@ -3,4 +3,4 @@
         materialized = "table"
     )
 }}
-{{ dbt_date.get_date_dimension('2015-01-01', '2022-12-31') }}
+{{ dbt_date.get_date_dimension('DATE \'2015-01-01\'', 'DATE \'2022-12-31\'') }}
